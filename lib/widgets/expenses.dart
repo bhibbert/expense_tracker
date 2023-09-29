c37b0fba5ccaeb7a7c3d7de95a74579c3046a09f
Brian Hibbert <brian@thehibberts.net>
2023-09-29 17:11:35 +0800

    Adding initial functionality

---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,53 @@
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Flutter Course',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: 'Lunch',
+        amount: 7.50,
+        date: DateTime.now(),
+        category: Category.food),
+    Expense(
+        title: 'Cinema',
+        amount: 15.00,
+        date: DateTime.now(),
+        category: Category.leisure),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add),
+          )
+        ],
+        title: const Text('Expense Tracker'),
+      ),
+      body: Column(
+        children: [
+          const Text('The Chart'),
+          Expanded(child: ExpensesList(expenses: _registeredExpenses)),
+        ],
+      ),
+    );
+  }
+}
